c67f08a629a580541009dba2407276a8816a83e5
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-10-08 02:18:21 +0300

    Uploaded Successfully

---
+++ OWNER.py
@@ -0,0 +1,11 @@
+# متطلبات التنصيب #
+OWNER = ["wwvvwt"]
+moord = 7728230165
+OWNER_NAME = "ᴊᴀᴋلـ™"
+BOT_TOKEN = "8429279183:AAGP6koXxa8_et_2ku3hxQwUzn-BeeIJikw"
+DATABASE = "mongodb+srv://Goldsex2005:islam2005@cluster0.rdjot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/qqvqqs/2551"
+GROUP = "https://t.me/xvwwvl"
+VIDEO = "https://graph.org/file/5563bd7719d2a49b66b42-8505021b7f80a08570.jpg"
+LOGS = "wwvvwt"
+
